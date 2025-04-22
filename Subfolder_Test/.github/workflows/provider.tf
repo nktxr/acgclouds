2c08869aff1075c35af41df702470b77d55d6455
nktxr <42946743+nktxr@users.noreply.github.com>
2025-04-22 11:33:39 +0000

    Test subfolder

---
+++ Subfolder_Test/.github/workflows/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  backend "s3" {
+    bucket = "localstack123459"
+    key    = "tfstate/localstack.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
