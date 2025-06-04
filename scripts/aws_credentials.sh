cf292a6e0baac226fac5be38de1c926f767690a1
nktxr <42946743+nktxr@users.noreply.github.com>
2025-06-04 14:28:21 +0000

    Test

--- scripts/aws_credentials.sh
+++ scripts/aws_credentials.sh
@@ -8,6 +8,7 @@ cat > .aws/credentials <<EOL
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+aws_session_token=${AWS_SESSION_TOKEN}
 EOL
 
 # Create the AWS config file to set the default region
